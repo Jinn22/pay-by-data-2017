986a2dc76390c7307c627d242f6b49dbc0a35543
Xiujin  Kong <xiujin.kong16@imperial.ac.uk>
2017-08-26 17:31:53 +0100

    Created the Table in the PBD Marketplace

--- pbdapi/scripts.js
+++ pbdapi/scripts.js
@@ -10,14 +10,10 @@ $(function(){
 
 
       $.each(app_infos,function(i,app_info){
-        //$apps.append('<li>name: ' + app_info.title + ', apk id: ' + app_info.apk_id + '</li>');
-        var $tr = $('<tr><th scope="row">2</th>').append(
-            $('<td>').text(app_info.title),
-            $('<td>').text(app_info.Duration),
-            $('<td>').text(app_info.apk_id)
-        ); //.appendTo('#records_table');
-        $('#app_table').append($tr);
-        console.log($tr.wrap('<p>').html());
+
+          var tr = '<tr><th scope="row">'+ i +'</th><td>'+app_info.title+'</td><td>'+app_info.Duration+'</td><td>'+app_info.apk_id+'</td></tr>';
+        $('#app_table').append(tr);
+        console.log(tr);
       });
     }
   });
